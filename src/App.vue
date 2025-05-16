6a17b96aad967cf09d993ede6e93ee5e975dd4dd
ngfchl <ngfchl@126.comn>
2025-05-16 11:19:05 +0800

    fix. 修复馒头同步导致 api 域名失效的 BUG

--- src/App.vue
+++ src/App.vue
@@ -366,7 +366,16 @@ async function getSiteData() {
     message.error('用户ID获取失败！')
     return false
   }
-  let data = `user_id=${user_id}&site=${siteInfo.value.name}&cookie=${cookie}&user_agent=${user_agent}&mirror=${document.location.origin}/`
+
+  /* 处理馒头域名 */
+  let host = `${document.location.origin}/`
+  if (host.includes("m-team")) {
+    host = host.replace("xp.", "api.")
+    host = host.replace("kp.", "api.")
+  }
+  let data = `user_id=${user_id}&site=${siteInfo.value.name}&cookie=${cookie}&user_agent=${user_agent}&mirror=${host}/`
+  /* 处理馒头域名结束 */
+
   if (mySiteId.value != 0) {
     data += `&id=${mySiteId.value}`
   }
