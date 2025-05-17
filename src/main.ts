e09c57ee133b2c95cd761ab4fbfd022327d85e41
ngfchl <ngfchl@126.comn>
2025-05-17 09:42:25 +0800

    delete. 删除网页样式重置代码

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {createApp} from 'vue';
 import './style.css';
-import 'ant-design-vue/dist/reset.css';
+// import 'ant-design-vue/dist/reset.css';
 import App from './App.vue';
 
 createApp(App).mount(
